d3776e1ef55c64192b037cff8e8571ece7280bd9
raj <3464136+RajendraMaharjan@users.noreply.github.com>
2022-05-17 13:56:34 -0500

    database configuration added for ProductDB

--- ProductService/src/main/java/productservice/service/ProductServiceInterface.java
+++ ProductService/src/main/java/productservice/service/ProductServiceInterface.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface ProductServiceInterface {
     Product getProduct(int productNumber);
+
     void insertProduct(Product p);
+
     List<Product> getAllProduct();
+
     void deleteProduct(Product p);
+
     void updateProduct(Product product);
+
     boolean hasProductWithQuantity(Product p, int quantity);
 }
