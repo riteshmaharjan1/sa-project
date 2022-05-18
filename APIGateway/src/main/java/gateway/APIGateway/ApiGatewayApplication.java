31348bc5c444216e0ef9737d96039d63ff7dde14
Ritesh Maharjan <ritesh@noveltytechnology.com>
2022-05-18 15:15:53 -0500

    addded Config file

--- APIGateway/src/main/java/gateway/APIGateway/ApiGatewayApplication.java
+++ APIGateway/src/main/java/gateway/APIGateway/ApiGatewayApplication.java
@@ -3,6 +3,9 @@ package gateway.APIGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,15 @@ public class ApiGatewayApplication {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class FallbackController{
+	@GetMapping("product-fallback")
+	Flux<Void> getFallback()
+	{
+		System.out.println("Fallback occured");
+		return Flux.empty();
+	}
 }
