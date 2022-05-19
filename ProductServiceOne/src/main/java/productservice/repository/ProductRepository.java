7d24caa02444bcf78f27b745ee2717ba7801ea6f
Ritesh Maharjan <ritesh@noveltytechnology.com>
2022-05-18 19:23:29 -0500

    removed unwanted project

--- ProductServiceOne/src/main/java/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import productservice.pojo.Product;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, Integer> {
-}
